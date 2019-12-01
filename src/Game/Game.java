2a919f1fda7c53f7c7ce61945813db4ba5eaf426
Shubh Sheth <shubhsheth8@gmail.com>
2019-12-01 16:14:31 -0600

    Assign Mines completed

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -2,23 +2,24 @@ package Game;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import Game.Cell;
 
 public class Game {
 	
-	
 	private static final int SIZE = 10;
-	
 	private int cellID = 0;
+	private int totalMines;
+	
 	private Cell[][] cells = new Cell[SIZE][SIZE];
 	
 	public Game() {
 		JFrame window = new JFrame("Minesweeper");
 		window.add(createGrid());
-		window.setSize(600, 600);
+		window.setSize(900, 900);
 
         assignMines();
         assignValues();
@@ -42,11 +43,22 @@ public class Game {
 	}
 	
 	private void assignMines() {
-		// To Do
-
 		
-		Cell cel = getCellByID(99);
-		cel.setValue("Test");
+		// Number of Mines - between 20 and 30
+		Random r = new Random();
+		this.totalMines = (int) r.nextInt((30 - 20) + 1) + 20;
+
+		// Assign Mines Randomly
+		ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 100; i++) {
+            list.add(new Integer(i));
+        }
+        Collections.shuffle(list); //unique random numbers from 0 - 99
+        
+        for ( int i = 0; i < this.totalMines; i++) {
+        	Cell cell = getCellByID(list.get(i));
+        	cell.setValue("X");
+        }
 	}
 	
 	private void assignValues() {
